f1e85b578f2dd2ba7477fa38a0a684fd98768a68
Matthieu Barthélémy <matthieuflobarthelemy@gmail.com>
2025-12-18 08:48:45 +0100

    ajout de main

--- worlds/desert.php
+++ worlds/desert.php
@@ -37,67 +37,68 @@ $idAvatar = $_GET['idAvatar'] ?? '';
             <?php endif; ?>
         </a-entity>
         <!-- Coffre près de l'entrée -->
-        <a-entity gltf-model="#chest_glb" position="-57 0 -4" scale="0.5 0.5 0.5" rotation="0 90 0"></a-entity>
+        <a-entity gltf-model="#chest_glb" position="-57 0 -4" scale="0.25 0.25 0.25" rotation="0 90 0"></a-entity>
 
         <a-entity environment="preset: egypt; groundYScale: 6; fog: 0.7"></a-entity>
         <a-entity light="type:ambient;intensity:1.0"></a-entity>
 
-        <a-entity gltf-model="#sphynx" position="20 5 20" scale="10 10 10" rotation="0 30 0"> </a-entity>
-        <a-entity gltf-model="#camel" position="10 0 0" scale="2 2 2" rotation="0 90 0"> </a-entity>
-        <a-entity gltf-model="#anubis" position="5 6 -2" scale="1 1 1" rotation="0 180 0"> </a-entity>
-        <a-entity gltf-model="#anubis" position="5 6 -20" scale="1 1 1" rotation="0 180 0"> </a-entity>
+        <a-entity gltf-model="#sphynx" position="-14.724 10.495 20" scale="1.5 1.5 1.5" rotation="0 30 0">
+        </a-entity>
+        <a-entity gltf-model="#camel" position="10 0 0" scale="1 1 1" rotation="0 90 0"> </a-entity>
+        <a-entity gltf-model="#anubis" position="5 6 -2" scale="0.5 0.5 0.5" rotation="0 180 0"> </a-entity>
+        <a-entity gltf-model="#anubis" position="5 6 -20" scale="0.5 0.5 0.5" rotation="0 180 0"> </a-entity>
 
-        <a-entity gltf-model="#arch" position="4 0 -10" scale="2 2 2" rotation="0 90 0"> </a-entity>
-        <a-entity gltf-model="#arch" position="2 0 -10" scale="2 2 2" rotation="0 90 0"> </a-entity>
-        <a-entity gltf-model="#arch" position="0 0 -10" scale="2 2 2" rotation="0 90 0"> </a-entity>
-        <a-entity gltf-model="#arch" position="-2 0 -10" scale="2 2 2" rotation="0 90 0"> </a-entity>
+        <a-entity gltf-model="#arch" position="4 0 -10" scale="1 1 1" rotation="0 90 0"> </a-entity>
+        <a-entity gltf-model="#arch" position="2 0 -10" scale="1 1 1" rotation="0 90 0"> </a-entity>
+        <a-entity gltf-model="#arch" position="0 0 -10" scale="1 1 1" rotation="0 90 0"> </a-entity>
+        <a-entity gltf-model="#arch" position="-2 0 -10" scale="1 1 1" rotation="0 90 0"> </a-entity>
 
-        <a-entity gltf-model="#fence" position="-4 0 -13" scale="3 3 3" rotation="0 0 0"> </a-entity>
-        <a-entity gltf-model="#fence" position="-4 0 -7" scale="3 3 3" rotation="0 0 0"> </a-entity>
-        <a-entity gltf-model="#coin" position="2 0 2" scale="0.5 0.5 0.5" rotation="0 0 0"> </a-entity>
+        <a-entity gltf-model="#fence" position="-4 0 -13" scale="1.5 1.5 1.5" rotation="0 0 0"> </a-entity>
+        <a-entity gltf-model="#fence" position="-4 0 -7" scale="1.5 1.5 1.5" rotation="0 0 0"> </a-entity>
+        <a-entity gltf-model="#coin" position="2 0 2" scale="0.25 0.25 0.25" rotation="0 0 0"> </a-entity>
 
-        <a-entity gltf-model="#pyramid" position="35 -10 -7" scale="8 8 8" rotation="0 133 0"> </a-entity>
-        <a-entity gltf-model="#roman_temple" position="-15 5 23" scale="15 15 15" rotation="0 180 0"> </a-entity>
-        <a-entity gltf-model="#roman_temple" position="-28 5 23" scale="15 15 15" rotation="0 180 0"> </a-entity>
-        <a-entity gltf-model="#roman_temple" position="-15 5 -40" scale="15 15 15" rotation="0 360 0"> </a-entity>
-        <a-entity gltf-model="#roman_temple" position="-28 5 -40" scale="15 15 15" rotation="0 360 0"> </a-entity>
+        <a-entity gltf-model="#pyramid" position="35 -10 -7" scale="4 4 4" rotation="0 133 0"> </a-entity>
+        <a-entity gltf-model="#roman_temple" position="-15 5 23" scale="7.5 7.5 7.5" rotation="0 180 0"> </a-entity>
+        <a-entity gltf-model="#roman_temple" position="-28 5 23" scale="7.5 7.5 7.5" rotation="0 180 0"> </a-entity>
+        <a-entity gltf-model="#roman_temple" position="-15 5 -40" scale="7.5 7.5 7.5" rotation="0 360 0"> </a-entity>
+        <a-entity gltf-model="#roman_temple" position="-28 5 -40" scale="7.5 7.5 7.5" rotation="0 360 0"> </a-entity>
 
-        <a-entity gltf-model="#sarcophagus" position="-56 -15,7  -15" scale="2 2 2" rotation="0 90 0"> </a-entity>
+        <a-entity gltf-model="#sarcophagus" position="-56 -15,7  -15" scale="1 1 1" rotation="0 90 0"> </a-entity>
 
-        <!-- Main -->
+        <!-- Main et caméra VR -->
         <a-entity id="rig">
             <a-camera id="camera" position="0 1.6 0"></a-camera>
-            <a-entity id="rhand" hand hand-controls="hand: right"></a-entity>
-            <a-entity id="lhand" hand teleport-controls="cameraRig: #rig; teleportOrigin: #camera; 
-button: trigger" hand-controls="hand: left"></a-entity>
+            <a-entity id="rhand" hand-controls="hand: right"></a-entity>
+            <a-entity id="lhand" hand-controls="hand: left"
+                teleport-controls="cameraRig: #rig; teleportOrigin: #camera; button: trigger; curveShootingSpeed: 10; type: parabolic">
+            </a-entity>
         </a-entity>
 
         <!-- Tente -->
-        <a-entity gltf-model="#tent" position="-17.268 2.448 -9.3" scale="5.021 5.021 5.021"
+        <a-entity gltf-model="#tent" position="-17.268 2.448 -9.3" scale="2.5105 2.5105 2.5105"
             rotation="0 180 0"></a-entity>
 
         <!-- Camels Respite -->
-        <a-entity gltf-model="#camels_respite" position="15 0 15" scale="0.5 0.5 0.5" rotation="0 0 0"></a-entity>
+        <a-entity gltf-model="#camels_respite" position="-54.908 0.266 32.358" scale="0.25 0.25 0.25"
+            rotation="0.000 135.204 0.000"></a-entity>
 
-        <!-- Groupe pyramide (positionné en -50 0 -7) et tourné de 90° autour de Y -->
-        <a-entity id="pyramid-group" position="-50 0 -7" rotation="0 270 0">
-            <a-triangle vertex-a="0 20 0" vertex-b="-10 0,7 -10" vertex-c="-3 0,7 -10"
-                material="color: #e9d25b; side:double"></a-triangle>
-            <a-triangle vertex-a="0 20 0" vertex-b="3 0,7 -10" vertex-c="10 0,7 -10"
-                material="color: #e9d25b; side:double"></a-triangle>
+        <!-- Temple Entrance -->
+        <a-entity gltf-model="#temple_entrance" position="-30 10 5" scale="0.05 0.05 0.05" rotation="0 0 0"></a-entity>
 
-            <a-triangle vertex-a="0 20 0" vertex-b="10 0,7 -10" vertex-c="10 0,7 10"
-                material="color: #e9d25b; side:double"></a-triangle>
-            <a-triangle vertex-a="0 20 0" vertex-b="10 0,7 10" vertex-c="-10 0,7 10"
-                material="color: #e9d25b; side:double"></a-triangle>
-            <a-triangle vertex-a="0 20 0" vertex-b="-10 0,7 10" vertex-c="-10 0,7 -10"
-                material="color: #e9d25b; side:double"></a-triangle>
+        <!-- Roman Temple -->
+        <a-entity gltf-model="#roman_temple_main" position="1.000 0 -45.007" scale="0.0085 0.0085 0.0085"
+            rotation="0 360 0"></a-entity>
 
-            <a-plane position="0 0,9 0" rotation="-90 0 0" width="20" height="20"
-                material="color: #e9d25b; side:double"></a-plane>
+        <!-- Oasis Trading Post -->
+        <a-entity gltf-model="#oasis_trading_post" position="-45.058 6.540 -36.086" scale="8.947 8.947 8.947"
+            rotation="0 134.529 0"></a-entity>
 
-            <!-- Modèle GLB (aligné au groupe) -->
-            <a-entity gltf-model="#piramid" position="0 1 0" scale="8 8 8" rotation="0 0 0"></a-entity>
+        <!-- Aztec Style Temple Kit -->
+        <a-entity gltf-model="#aztec_style_temple_kit" position="26.793 1.627 54.707" scale="6.122 6.122 6.122"
+            rotation="0 45 0"></a-entity>
+
+        <!-- Modèle GLB (aligné au groupe) -->
+        <a-entity gltf-model="#piramid" position="0 1 0" scale="4 4 4" rotation="0 0 0"></a-entity>
         </a-entity>
 
         <a-assets>
@@ -117,6 +118,11 @@ button: trigger" hand-controls="hand: left"></a-entity>
             <a-asset-item id="chest_glb" src="../assets/modelAvatar/chest.glb"></a-asset-item>
             <a-asset-item id="tent" src="../assets/modelAvatar/Tent.glb"></a-asset-item>
             <a-asset-item id="camels_respite" src="../assets/modelAvatar/camels_respite.glb"></a-asset-item>
+            <a-asset-item id="temple_entrance" src="../assets/modelAvatar/temple_entrance.glb"></a-asset-item>
+            <a-asset-item id="roman_temple_main" src="../assets/modelAvatar/roman_temple.glb"></a-asset-item>
+            <a-asset-item id="oasis_trading_post" src="../assets/modelAvatar/oasis_trading_post.glb"></a-asset-item>
+            <a-asset-item id="aztec_style_temple_kit"
+                src="../assets/modelAvatar/aztec_style_temple_kit.glb"></a-asset-item>
             <?php if ($modelAvatar): ?>
                 <a-asset-item id="avatar" src="../<?php echo htmlspecialchars($modelAvatar); ?>"></a-asset-item>
             <?php endif; ?>
